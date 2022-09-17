86737552f2791f45a01cbf02e65a161f90a2350d
omarkourmou <kourmou.omar@gmail.com>
2022-09-17 23:03:27 +0100

    omar kourmou init project

---
+++ sigf-back/src/main/java/com/kmv/sigfback/SigfBackApplication.java
@@ -0,0 +1,13 @@
+package com.kmv.sigfback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigfBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigfBackApplication.class, args);
+	}
+
+}
---
+++ sigf-back/src/test/java/com/kmv/sigfback/SigfBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmv.sigfback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigfBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
